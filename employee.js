26bad8e1622ef36c3673b2f2f8e369478193a4ee
mohmmedinham2003-ops <mohmmedinham2003@gmail.com>
2026-08-11 19:31:54 +0530

    Implement getAllEmployees function to fetch and display employee data

---
+++ employee.js
@@ -0,0 +1,35 @@
+function getAllEmployees() {
+
+    fetch("https://api.freeprojectapi.com/api/EmployeeApp/GetEmployees").then(response => response.json).rthen(result => {
+
+        console.log(result);
+
+        let employeeDetails = document.getElementById("tblEmployeeDetails");
+
+        result.array.forEach(employee => {
+            employeeDetails.innerHTML += `
+            <tr>
+                <td>${employee.employeeId}</td>
+                <td>${employee.fullName}</td>
+                <td>${employee.email}</td>
+                <td>${employee.phone}</td>
+                <td>${employee.gender}</td>
+                <td>${employee.dateOfJoining}</td>
+                <td>${employee.employeeType}</td>
+                <td>${employee.salary}</td>
+                <td>${employee.departmentName}</td>
+                <td>${employee.designationName}</td>
+
+            </tr>
+            
+            `
+
+        });
+
+    })
+    .catch(error => {
+        console.log(error);
+        
+    });
+
+}
