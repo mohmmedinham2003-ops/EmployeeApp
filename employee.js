ddb96f03e68ba8bb8281b0edf9e95ca4e56b977f
mohmmedinham2003-ops <mohmmedinham2003@gmail.com>
2026-08-13 21:57:54 +0530

    Implement updateEmployee functionality and initialize selectedEmployeeId for getEmployeeById

--- employee.js
+++ employee.js
@@ -1,3 +1,5 @@
+let selectedEmployeeId = 0;
+
 function getAllEmployees() {
 
     fetch("https://api.freeprojectapi.com/api/EmployeeApp/GetEmployees").then(response => response.json()).then(result => {
@@ -154,48 +156,51 @@ function createEmployee() {
 }
 
 
-// function updateEmployee() {
+function updateEmployee() {
 
-//     let employee = {
-//         employeeId: 8402,
-//         fullName: "Ahmed Updated",
-//         email: "ahmed@gmail.com",
-//         phone: "0771234567",
-//         gender: "Male",
-//         dateOfJoining: "2026-08-12T00:00:00",
-//         departmentId: 1,
-//         designationId: 1,
-//         employeeType: "Permanent",
-//         salary: 60000
-//     };
+    let employee = {
+        employeeId: 8402,
+        fullName: "Ahmed Updated",
+        email: "ahmed@gmail.com",
+        phone: "0771234567",
+        gender: "Male",
+        dateOfJoining: "2026-08-12T00:00:00",
+        departmentId: 1,
+        designationId: 1,
+        employeeType: "Permanent",
+        salary: 60000
+    };
 
-//     fetch("https://api.freeprojectapi.com/api/EmployeeApp/UpdateEmployee?id=8402", {
+    fetch("https://api.freeprojectapi.com/api/EmployeeApp/UpdateEmployee?id=8402", {
 
-//         method: "PUT",
+        method: "PUT",
 
-//         headers: {
-//             "Content-Type": "application/json"
-//         },
+        headers: {
+            "Content-Type": "application/json"
+        },
 
-//         body: JSON.stringify(employee)
+        body: JSON.stringify(employee)
 
-//     })
-//         .then(response => response.json())
-//         .then(result => {
+    })
+        .then(response => response.json())
+        .then(result => {
 
-//             console.log(result);
+            console.log(result);
 
-//         })
-//         .catch(error => {
+        })
+        .catch(error => {
 
-//             console.log(error);
+            console.log(error);
 
-//         });
-// }
-// updateEmployee();
+        });
+}
+updateEmployee();
 
 
 function getEmployeeById(employeeId) {
+
+    selectedEmployeeId = employeeId;
+
     fetch(`https://api.freeprojectapi.com/api/EmployeeApp/${employeeId}`)
         .then(response => response.json())
         .then(result => {
@@ -230,7 +235,6 @@ function getEmployeeById(employeeId) {
 
 }
 
-getEmployeeById(8402);
 
 function deleteEmployee(employeeId) {
 
