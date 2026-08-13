5f72dcadd419dc5434db13a9dea2bf0480652bf7
mohmmedinham2003-ops <mohmmedinham2003@gmail.com>
2026-08-13 21:45:28 +0530

    Comment out updateEmployee function and implement getEmployeeById functionality

--- employee.js
+++ employee.js
@@ -154,48 +154,65 @@ function createEmployee() {
 }
 
 
-function updateEmployee() {
+// function updateEmployee() {
 
-    let employee = {
-        employeeId: 8402,
-        fullName: "Ahmed Updated",
-        email: "ahmed@gmail.com",
-        phone: "0771234567",
-        gender: "Male",
-        dateOfJoining: "2026-08-12T00:00:00",
-        departmentId: 1,
-        designationId: 1,
-        employeeType: "Permanent",
-        salary: 60000
-    };
+//     let employee = {
+//         employeeId: 8402,
+//         fullName: "Ahmed Updated",
+//         email: "ahmed@gmail.com",
+//         phone: "0771234567",
+//         gender: "Male",
+//         dateOfJoining: "2026-08-12T00:00:00",
+//         departmentId: 1,
+//         designationId: 1,
+//         employeeType: "Permanent",
+//         salary: 60000
+//     };
 
-    fetch("https://api.freeprojectapi.com/api/EmployeeApp/UpdateEmployee?id=8402", {
+//     fetch("https://api.freeprojectapi.com/api/EmployeeApp/UpdateEmployee?id=8402", {
 
-        method: "PUT",
+//         method: "PUT",
 
-        headers: {
-            "Content-Type": "application/json"
-        },
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
 
-        body: JSON.stringify(employee)
+//         body: JSON.stringify(employee)
 
-    })
+//     })
+//         .then(response => response.json())
+//         .then(result => {
+
+//             console.log(result);
+
+//         })
+//         .catch(error => {
+
+//             console.log(error);
+
+//         });
+// }
+// updateEmployee();
+
+
+function getEmployeeById(employeeId){
+    fetch(`https://api.freeprojectapi.com/api/EmployeeApp/${employeeId}`)
         .then(response => response.json())
         .then(result => {
 
             console.log(result);
 
+            docu
+
         })
         .catch(error => {
-
             console.log(error);
-
         });
+
+
 }
-updateEmployee();
 
-
-function
+getEmployeeById(8402);
 
 function deleteEmployee(employeeId) {
 
